05fc1d1b80389fa636cb9237e92a329a52d0dc68
Tim <timtran007@gmail.com>
2022-12-05 00:26:42 -0600

    commit tree 6e38a6947b9461a6ba41294ba4c71578a54a6d9b

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears(
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN,
+    id INTEGER PRIMARY KEY
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 5, "M", "Brown", "Nice", 1), 
+("Rowdy", 2, "M", "Brown", "Playful", 1), 
+("Tabitha", 3, "F", "Brown", "Nice", 1), 
+("Sergeant Brown", 6, "M", "Brown", "Nice", 1), 
+("Melissa", 1, "F", "Brown", "affectionate", 1), 
+("Grinch", 8, "M", "Green", "Grumpy", 1), 
+("Wendy", 2, "F", "Brown", "Nice", 1), 
+(NULL, 5, "M", "Brown", "trusting", 1);
